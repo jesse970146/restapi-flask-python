407807e0bcbfff0010a835df973c377e9d07351e
jesse970146 <jesse970146@gmail.com>
2023-12-04 23:22:06 +0800

    doc(app.py) fixed bug

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
-    db.init_app(app)
+    # db.init_app(app)
     migrate =Migrate(app, db)
 
     api=  Api(app)
@@ -100,8 +100,8 @@ def create_app(db_url=None):
             401,
         )
 
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
